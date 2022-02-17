d86c0007c4834d4bfbc30924d19ff870d14596f2
Avi Rana <68737072+Avi-Rana-1718@users.noreply.github.com>
2022-02-17 13:40:42 +0530

    Create help.js

---
+++ bot/V2/commands/help.js
@@ -0,0 +1,18 @@
+
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('help')
+		.setDescription('View list of all available commands'),
+  
+	async execute(interaction) {
+    let help = new MessageEmbed()
+    .setTitle("Megumin Help")
+      .setDescription("Megumin now uses [slash commands](https://discord.com/blog/slash-commands-are-here), visit [V2 announcement](https://avi-rana-1718.github.io/Megumin/blog/V2) for more info.\n\n:newspaper: **Announcements**\n-Bot rebranded from `Tamako` to `Megumin`\n-V2 now out\n\nList of all commands\n-help\n-anime\n-advance\n-info")
+  .setColor("#E41F7B")
+      .setTimestamp()
+		return interaction.reply({ embeds: [help] });
+	},
+};
