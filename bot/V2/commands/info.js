463a85950130d733e8290a162ef6a87ab3fc6f3a
Avi Rana <68737072+Avi-Rana-1718@users.noreply.github.com>
2022-04-26 20:12:49 +0530

    Update info.js

--- bot/V2/commands/info.js
+++ bot/V2/commands/info.js
@@ -2,6 +2,9 @@ const api = require('gogoanime-xp');
 const wait = require('util').promisify(setTimeout);
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed } = require('discord.js');
+const { MessageActionRow, MessageButton } = require('discord.js');
+ const { getInfoFromName } = require('myanimelists');
+
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -20,18 +23,32 @@ module.exports = {
     var title = res[0].id;
 var count = res[0].totalEpisodes;
 var ep = interaction.options.getNumber('episode');
+
+  getInfoFromName(res[0].title)
+    .then(result => {
+      const url = result.url;
       
+          const row = new MessageActionRow()
+			.addComponents(
+				new MessageButton()
+					.setLabel('View on MyAnimeList')
+					.setStyle('LINK')
+        .setURL(url),
+			);
+    
 let info = new MessageEmbed()
     .setTitle(res[0].title)
     .setDescription(res[0].synopsis)
-    .setThumbnail(res[0].img)
+.setThumbnail(encodeURI(res[0].img))
     .setColor("#E41F7B")
     .addFields(
 		{ name: 'Genre', value: `${res[0].genres}`, inline: true },
 		{ name: 'Episodes', value: `${count}`, inline: true },
 	{ name: 'Status', value: `${res[0].status}`, inline: true }
   )
-     interaction.editReply({ embeds: [info] });
+
+     interaction.editReply({ embeds: [info], components: [row]  });
+ }).catch(error => console.log(error));
   }).catch(err => {
        let error = new MessageEmbed()
        .setTitle("**ERROR**")
