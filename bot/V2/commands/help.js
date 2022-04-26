d9df9a2caa2c2c5c711e082bdad118c700fc4c03
Avi Rana <68737072+Avi-Rana-1718@users.noreply.github.com>
2022-04-26 20:12:28 +0530

    Update help.js

--- bot/V2/commands/help.js
+++ bot/V2/commands/help.js
@@ -10,7 +10,7 @@ module.exports = {
 	async execute(interaction) {
     let help = new MessageEmbed()
     .setTitle("Megumin Help")
-      .setDescription("Megumin now uses [slash commands](https://discord.com/blog/slash-commands-are-here), visit [V2 announcement](https://avi-rana-1718.github.io/Megumin/blog/V2) for more info.\n\n:newspaper: **Announcements**\n-Bot rebranded from `Tamako` to `Megumin`\n-V2 now out\n\nList of all commands\n-help\n-anime\n-advance\n-info")
+      .setDescription("Megumin now uses [slash commands](https://discord.com/blog/slash-commands-are-here), visit [V2 announcement](https://avi-rana-1718.github.io/Megumin/blog/V2) for more info.\n\n:newspaper: **Announcements**\n-New /search & /manual commands\n-Bot rebranded from `Tamako` to `Megumin`\n-V2 now out\n\nList of all commands\n-help\n-anime\n-advance\n-info\n-search\n-manual")
   .setColor("#E41F7B")
       .setTimestamp()
 		return interaction.reply({ embeds: [help] });
